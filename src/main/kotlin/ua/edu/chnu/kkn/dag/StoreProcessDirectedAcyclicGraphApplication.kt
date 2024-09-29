fd43b8962ab976700041b413873710e1d4f9e56d
Edgar Vatamanitsa <edgar.vatamanitsa@gmail.com>
2024-09-29 15:37:00 +0300

    Initial commit.

---
+++ src/main/kotlin/ua/edu/chnu/kkn/dag/StoreProcessDirectedAcyclicGraphApplication.kt
@@ -0,0 +1,11 @@
+package ua.edu.chnu.kkn.dag
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StoreProcessDirectedAcyclicGraphApplication
+
+fun main(args: Array<String>) {
+	runApplication<StoreProcessDirectedAcyclicGraphApplication>(*args)
+}
