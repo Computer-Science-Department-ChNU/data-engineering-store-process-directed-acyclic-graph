23c80ee7ebf11d77a83f52afe3820b661bc20576
Edgar Vatamanitsa <edgar.vatamanitsa@gmail.com>
2025-01-05 15:57:49 +0200

    Refactored individual service saving functions. Fixed docker compose file.

--- src/main/kotlin/ua/edu/chnu/kkn/dag/familytree/IndividualService.kt
+++ src/main/kotlin/ua/edu/chnu/kkn/dag/familytree/IndividualService.kt
@@ -4,9 +4,9 @@ import org.folg.gedcom.model.GedcomTag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import ua.edu.chnu.kkn.dag.familytree.common.*
 import ua.edu.chnu.kkn.dag.familytree.neo.IndividualNeo4jRepository
 import ua.edu.chnu.kkn.dag.familytree.neo.IndividualNeo4jNode
-import ua.edu.chnu.kkn.dag.familytree.common.isIndividual
 
 @Service
 class IndividualService {
@@ -20,49 +20,47 @@ class IndividualService {
     }
 
     private fun saveIndividuals(gedComTags: List<GedcomTag>) {
-        println("------------Begin saveAll-------------")
         val individuals = gedComTags
             .filter { it.isIndividual() }
             .map(this::toIndividual)
             .sortedBy { it.id }
         individualNeo4jRepository.saveAll(individuals)
-
-        println("End saveAll")
-
         saveIndividualRelationships(gedComTags, individuals)
-
-        println("End saveAll with connection")
-
-
     }
 
-    private fun saveIndividualRelationships(gedComTags: List<GedcomTag>, individualNeo4jNodes: List<IndividualNeo4jNode> ){
+    private fun saveIndividualRelationships(
+        gedComTags: List<GedcomTag>,
+        individuals: List<IndividualNeo4jNode>
+    ) {
         val individualsWithRelations = mutableListOf<IndividualNeo4jNode>()
-        for (tag in gedComTags) {
-            if (tag.tag == "FAM") {
-                val tempFather = individualNeo4jNodes.find { it.realId == tag.children.getOrNull(0)?.ref && tag.children[0].tag == "HUSB" }
-                var tempMother : IndividualNeo4jNode? = null
-                if (tag.children[0].tag == "WIFE"){
-                    tempMother = individualNeo4jNodes.find { it.realId == tag.children[0].ref }
-                }
-                else if (tag.children.size > 1 && tag.children[1]?.tag == "WIFE") {
-                    tempMother = individualNeo4jNodes.find { it.realId == tag.children[1].ref }
-                }
-                tag.children.filter { it.tag == "CHIL" }.forEach {childTag ->
-                    val individ = individualNeo4jNodes.find { it.realId == childTag.ref }
-                    val individCopy = individ?.copy()
-
-                    individCopy?.father = tempFather
-                    individCopy?.mother = tempMother
-                    individCopy?.let { individualsWithRelations.add(it) }
-                    println("Save with connections ${individ?.id}" )
+        gedComTags.asSequence()
+            .filter { it.isFamily() }
+            .forEach { tag ->
+                val father = tag.children
+                    .filter { it.isHusband() }
+                    .firstNotNullOfOrNull { children ->
+                        individuals.find { it.realId == children.ref }
+                    }
+                val mother: IndividualNeo4jNode? = tag.children
+                    .filter { it.isWife() }
+                    .firstNotNullOfOrNull { children ->
+                        individuals.find { it.realId == children.ref }
+                    }
+                tag.children.filter { it.isChild() }.forEach { childTag ->
+                    val individual = individuals.find { it.realId == childTag.ref }
+                    val individualCopy = individual?.copy()
+                    individualCopy?.father = father
+                    individualCopy?.mother = mother
+                    individualCopy?.let { individualsWithRelations.add(it) }
                 }
             }
-        }
         individualNeo4jRepository.saveAll(individualsWithRelations)
     }
 
-
-
-    private fun toIndividual(tag: GedcomTag) = IndividualNeo4jNode(id = tag.id.replace("I", "").toInt(), realId = tag.id, name = tag.children[0].value)
+    private fun toIndividual(tag: GedcomTag) =
+        IndividualNeo4jNode(
+            id = tag.id.replace("I", "").toInt(),
+            realId = tag.id,
+            name = tag.children[0].value
+        )
 }
--- src/main/kotlin/ua/edu/chnu/kkn/dag/familytree/common/GedcomExtensions.kt
+++ src/main/kotlin/ua/edu/chnu/kkn/dag/familytree/common/GedcomExtensions.kt
@@ -3,3 +3,11 @@ package ua.edu.chnu.kkn.dag.familytree.common
 import org.folg.gedcom.model.GedcomTag
 
 fun GedcomTag.isIndividual() = this.tag == "INDI"
+
+fun GedcomTag.isFamily() = this.tag == "FAM"
+
+fun GedcomTag.isHusband() = this.tag == "HUSB"
+
+fun GedcomTag.isWife() = this.tag == "WIFE"
+
+fun GedcomTag.isChild() = this.tag == "CHIL"
