4d630836b652d94a226183ee4adc4abfab049fbe
Edgar Vatamanitsa <edgar.vatamanitsa@gmail.com>
2025-02-03 22:01:06 +0200

    Added migration file.

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE individuals (
+                          user_id VARCHAR (10) PRIMARY KEY,
+                          name VARCHAR (50) NOT NULL,
+                          sex VARCHAR (10) NOT NULL,
+                          birth_date DATE,
+                          death_date DATE
+);
